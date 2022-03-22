ff8d1a3a60ddb4317b34bbac68bea966d6c206e5
Erik Baer <erik.baer@timetoact.de>
2022-03-22 16:48:46 +0100

    add interaces and approve token tx

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 1. Swap our ETH for WETH
-2. Deposite some WETH into AAVE
+2. Deposite some ETH (WETH) into AAVE
 3. Borrow some aset with the ETH collateral
     1. Sell that borrowed asset. (Short selling)
 4. Repay everything back
--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -9,3 +9,26 @@ def main():
     erc20_address = config["networks"][network.show_active()]["weth_token"]
     if network.show_active() in ["mainnet-fork"]:
         get_weth()
+    lending_pool = get_lending_pool()
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    # get ABI (opr create yourself)
+    # get Address
+    print("Approving ERC20 token...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved!")
+    return tx
+
+
+def get_lending_pool():
+    # get Address
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    # get ABI
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
